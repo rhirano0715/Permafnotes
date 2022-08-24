08e5140c714767368ad9a2e47185f74ff3aece20
rhiarno0715 <hiarno.ryuko.0715@gmail.com>
2022-08-24 11:23:49 +0900

    add

---
+++ src/permafnotes/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using permafnotes;
+using PermafnotesDomain.Services;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+});
+
+builder.Services.AddGraphClient("https://graph.microsoft.com/User.Read", "https://graph.microsoft.com/Files.ReadWrite");
+
+builder.Logging.SetMinimumLevel(LogLevel.Trace);
+
+builder.Services.AddScoped<NoteService>();
+
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+
+await builder.Build().RunAsync();
